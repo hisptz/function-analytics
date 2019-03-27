f9fc65f132faa3fe7c7a9b1816e5c1332a9723f7
Vincent P. Minde <vincentminde@gmail.com>
2019-03-27 17:37:52 +0300

    Added analytics sanitization

--- src/impl/analytics.js
+++ src/impl/analytics.js
@@ -2,6 +2,12 @@ import { Fetcher } from '../core/fetcher';
 
 export class Analytics extends Fetcher {
 
+  constructor(oldAnalytics = true) {
+    super();
+    this.postProcess((data) => {
+      return this.standardizeAnalytics(data, oldAnalytics);
+    });
+  }
   setParameters(parameters) {
     if (parameters.dx) {
       this.setData(parameters.dx);
@@ -30,8 +36,133 @@ export class Analytics extends Fetcher {
     return this;
   }
 
+  standardizeAnalytics(analyticsObject, preferNormalStructure = true) {
+    // if Serverside Event clustering do nothing
+    if (analyticsObject.count) {
+      return analyticsObject;
+    }
+    let sanitizedAnalyticsObject = {
+      headers: [],
+      metaData: {
+        dimensions: {},
+        names: {},
+        dx: [],
+        pe: [],
+        ou: [],
+        co: []
+      },
+      rows: []
+    };
+
+    if (analyticsObject) {
+
+      /**
+       * Check headers
+       */
+      if (analyticsObject.headers) {
+        analyticsObject.headers.forEach(function (header) {
+          try {
+            let newHeader = header;
+
+            sanitizedAnalyticsObject.headers.push(newHeader);
+          } catch (e) {
+            console.warn('Invalid header object');
+          }
+        });
+      }
+
+      /**
+       * Check metaData
+       */
+      if (analyticsObject.metaData) {
+        try {
+          let sanitizedMetadata = this.getSanitizedAnalyticsMetadata(analyticsObject.metaData, preferNormalStructure);
+
+          sanitizedAnalyticsObject.metaData = sanitizedMetadata;
+        } catch (e) {
+          console.warn('Invalid metadata object');
+        }
+      }
+
+      /**
+       * Check rows
+       */
+      if (analyticsObject.rows) {
+        sanitizedAnalyticsObject.rows = analyticsObject.rows;
+      }
+    }
+    sanitizedAnalyticsObject.height = analyticsObject.height;
+    sanitizedAnalyticsObject.width = analyticsObject.width;
+    return sanitizedAnalyticsObject;
+  }
+
+  getSanitizedAnalyticsMetadata(analyticMetadata, preferNormalStructure) {
+    let sanitizedMetadata = {
+      dimensions: {},
+      names: {},
+      dx: [],
+      pe: [],
+      ou: [],
+      co: []
+    };
+
+    if (analyticMetadata) {
+      /**
+       * Get metadata names
+       */
+      if (analyticMetadata.names) {
+        sanitizedMetadata.names = analyticMetadata.names;
+      } else if (analyticMetadata.items) {
+
+        let metadataNames = {};
+
+        for (let metadataItemKey in analyticMetadata.items) {
+          metadataNames[metadataItemKey] = analyticMetadata.items[metadataItemKey].name;
+        }
+
+        sanitizedMetadata['names'] = metadataNames;
+      }
+
+      /**
+       * Get metadata dimensions
+       */
+      if (analyticMetadata.dimensions) {
+        if (!preferNormalStructure) {
+          sanitizedMetadata['dimensions'] = analyticMetadata.dimensions;
+        } else {
+          sanitizedMetadata.dimensions = analyticMetadata.dimensions;
+          sanitizedMetadata.dx = analyticMetadata.dimensions.dx;
+          sanitizedMetadata.ou = analyticMetadata.dimensions.ou;
+          sanitizedMetadata.pe = analyticMetadata.dimensions.pe;
+          sanitizedMetadata.co = analyticMetadata.dimensions.co;
+        }
+      } else {
+        let metadataDimensions = {};
+
+        for (let metadataKey in analyticMetadata.dimensions) {
+          if (analyticMetadata.hasOwnProperty(metadataKey)) {
+            if (metadataKey !== 'names') {
+              metadataDimensions[metadataKey] = analyticMetadata.dimensions[metadataKey];
+            }
+          }
+        }
+
+        if (!preferNormalStructure) {
+          sanitizedMetadata['dimensions'] = metadataDimensions;
+        } else {
+          sanitizedMetadata.dx = metadataDimensions.dx;
+          sanitizedMetadata.ou = metadataDimensions.ou;
+          sanitizedMetadata.pe = metadataDimensions.pe;
+          sanitizedMetadata.co = metadataDimensions.co;
+        }
+      }
+    }
+
+    return sanitizedMetadata;
+  }
+
   get url() {
-    var url = 'analytics?';
+    let url = 'analytics?';
 
     if (this.dx) {
       url += 'dimension=dx:' + this.dx;
