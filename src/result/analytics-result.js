612e5eb5b38f23550ccf8a22163254ac1e9bc82a
Vincent P. Minde <vincentminde@gmail.com>
2019-06-07 17:10:56 +0300

    Added headers into the analytics results'

--- src/result/analytics-result.js
+++ src/result/analytics-result.js
@@ -1,6 +1,76 @@
 import {Row} from './row';
 
 /**
+ * This represents the Analytics header
+ *
+ */
+export class AnalyticsHeader {}
+/**
+ * This represents the Analytics Headers
+ *
+ * @extends Array
+ */
+export class AnalyticsHeaders extends Array {
+  constructor(data) {
+    super(...data);
+    Object.setPrototypeOf(this, Object.create(AnalyticsHeaders.prototype));
+  }
+
+  /**
+   * Gets the data analytics header
+   *
+   * @returns {AnalyticsHeader}
+   */
+  get dx() {
+    return this.getHeader('dx');
+  }
+
+  /**
+   * Gets the period analytics header
+   *
+   * @returns {AnalyticsHeader}
+   */
+  get pe() {
+    return this.getHeader('pe');
+  }
+
+  /**
+   * Gets the organisation unit analytics header
+   *
+   * @returns {AnalyticsHeader}
+   */
+  get ou() {
+    return this.getHeader('ou');
+  }
+
+  /**
+   * Gets the value analytics header
+   *
+   * @returns {AnalyticsHeader}
+   */
+  get value() {
+    return this.getHeader('value');
+  }
+
+  /**
+   * Gets the header of a parameter
+   * @param id
+   * @returns {AnalyticsHeader}
+   */
+  getHeader(id) {
+    let returnHeader;
+
+    this.forEach((header, index) => {
+      if (header.name === id) {
+        returnHeader = header;
+        returnHeader.index = index;
+      }
+    });
+    return returnHeader;
+  }
+}
+
+/**
  * This represents the Analytics Results
  *
  */
@@ -19,8 +89,19 @@ export class AnalyticsResult {
    *
    * @returns {Array} - DHIS Analytics headers
    */
+  /*
   get headers() {
     return this._data.headers;
+  }
+  */
+
+  /**
+   * Gets the Analytics Headers Array
+   *
+   * @returns {AnalyticsHeaders}
+   */
+  get headers() {
+    return new AnalyticsHeaders(this._data.headers);
   }
 
   /**
